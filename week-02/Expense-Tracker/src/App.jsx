03680363f6bc648d90e03313865ebc5d8f9bf71b
Farzana <akbarifarzana77@gmail.com>
2026-01-26 18:04:36 +0400

    Refactor App layout and clean up styles
    Updated App.jsx to improve structure with sections for adding and listing expenses, fixed header and class names, and removed placeholder text. Simplified App.css font stack and margin. Removed unused global styles from index.css.

--- week-02/Expense-Tracker/src/App.jsx
+++ week-02/Expense-Tracker/src/App.jsx
@@ -1,21 +1,35 @@
- import './App.css'
+import "./App.css";
 
- function App() {
+function App() {
   return (
-      <div className='page'>
-    <header className='header'>
-      <div>
-        <h1 className='Expenese Tracker Application'></h1>
-        <p className='subtitle'>week 1 & week 2 practice project</p>
-      </div>
-    </header>
+    <div className="page">
+      <header className="header">
+        <div>
+          <h1 className="title">Expense Tracker Application</h1>
+          <p className="subtitle">week 1 & week 2 practice project</p>
+        </div>
+      </header>
 
-  </div>
+      <section className="card">
+        <div className="cardheader"> 
+          <h2 className="sectionTitle">Add Expense</h2>  
+        </div>
+        <div className="cardBody">
+         <p>Form will be here </p> 
+           </div>
+      </section>
 
-  )
-
-
+      <section className="card">
+        <div className="cardheader"> 
+          <h2 className="sectionTitle">Expenses</h2>  
+        </div>
+        <div className="cardBody">
+         <p>Form will be here </p> 
+           </div>
+      </section>
+    
+</div>
+  );
 }
 
-  
-export default App
+export default App;
