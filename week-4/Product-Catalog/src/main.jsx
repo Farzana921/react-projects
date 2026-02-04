db144fe3eda583c7b7d11b727ff54c987cf5a732
Farzana <akbarifarzana77@gmail.com>
2026-02-04 15:07:08 +0400

    Update main.jsx

--- week-4/Product-Catalog/src/main.jsx
+++ week-4/Product-Catalog/src/main.jsx
@@ -1,38 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-
 import "./index.css";
 
-import Navbar from "./components/Navbar";
+import RootLayout from "./pages/RootLayout";
 import Home from "./pages/Home";
 import ProductsLayout from "./pages/ProductsLayout";
 import ProductsList from "./pages/ProductsList";
 import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
 
-function AppLayout() {
-  return (
-    <div>
-      <Navbar />
-      <div style={{ maxWidth: 900, margin: "0 auto" }}>
-        {/* child routes will render below navbar */}
-      </div>
-    </div>
-  );
-}
-
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <Navbar />
-        <div style={{ maxWidth: 900, margin: "0 auto" }}>
-          {/* Home will render here */}
-        </div>
-      </div>
-    ),
+    element: <RootLayout />,
     children: [
       { index: true, element: <Home /> },
       {
