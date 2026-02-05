0a7b3b21dc9e4fedc70c8bb5671713926e7d1ad6
Farzana <akbarifarzana77@gmail.com>
2026-02-05 15:39:30 +0400

    initial commit

--- week-04/Product-Catalog/src/components/Navbar.jsx
+++ week-04/Product-Catalog/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-import { NavLink} from "react-router";
-export default function Navbar() {
-    const linkstyle = ({isActive }) => ({
-        textDecoration: isActive ? "underline" : "none",
-        fontWeight : isActive ? "800" : "500",
-        marginRight: 12,
-    });
+import { NavLink } from "react-router";
 
-    return (
-        <nav style={{padding: 12, borderBotton: "1px solid #ddd"}}>
-            <NavLink to="/" style={linkStyle} end>
-            Home
-            </NavLink>
-            <NavLink to="/products" style={linkstyle}>
-             Products     
-            </NavLink>
-        </nav>
-    );
-    
+export default function Navbar() {
+  const linkStyle = ({ isActive }) => ({
+    textDecoration: isActive ? "underline" : "none",
+    fontWeight: isActive ? "800" : "500",
+    marginRight: 12,
+  });
+
+  return (
+    <nav style={{ padding: 12, borderBottom: "1px solid #ddd" }}>
+      <NavLink to="/" style={linkStyle} end>
+        Home
+      </NavLink>
+
+      <NavLink to="/products" style={linkStyle}>
+        Products
+      </NavLink>
+    </nav>
+  );
 }
